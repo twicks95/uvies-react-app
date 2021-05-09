e2ccb016eaf6a5a762890d7fbe5c39f05368bfc9
twicks95 <teguh.wck95@gmail.com>
2021-05-09 09:39:30 +0700

    Update axios utility

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,43 @@
 import axios from "axios";
 
-const axiosApiIntances = axios.create({
+const axiosApiInstances = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-export default axiosApiIntances;
+// Add a request interceptor
+axiosApiInstances.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    // set bagian headers
+    config.headers = {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    };
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosApiInstances.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 403) {
+      alert("Please login first!");
+      localStorage.clear();
+      window.location.href = "/sign-in";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosApiInstances;
