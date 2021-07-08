de5844518a0ceddb6b800ea189a78f9568eddd3b
twicks95 <teguh.wck95@gmail.com>
2021-07-08 09:32:23 +0700

    Update movie & user reducers and add premiere reducer

--- src/redux/reducers/movie.js
+++ src/redux/reducers/movie.js
@@ -1,9 +1,11 @@
 const initialState = {
-  data: [],
+  movies: [],
+  pagination: {},
+  upcomingMovies: [],
   isLoading: false,
   isError: false,
+  movieData: {},
   msg: "",
-  pagination: {},
 };
 
 const movie = (state = initialState, action) => {
@@ -14,20 +16,167 @@ const movie = (state = initialState, action) => {
         isLoading: true,
         isError: false,
       };
-    case "GET_MOVIE_FULFILLED":
+    case "GET_MOVIES_FULFILLED":
       return {
         ...state,
-        data: action.payload.data.data,
+        movies: action.payload.data.data,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+        pagination: action.payload.data.pagination,
       };
-    case "GET_MOVIE_REJECTED":
+    case "GET_MOVIES_REJECTED":
       return {
         ...state,
-        data: [],
+        movies: [],
         isLoading: false,
         isError: true,
         msg: action.payload.response.data.msg,
       };
-
+    case "GET_UPCOMING_MOVIES_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "GET_UPCOMING_MOVIES_FULFILLED":
+      return {
+        ...state,
+        upcomingMovies: action.payload.data.data,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+      };
+    case "GET_UPCOMING_MOVIES_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "CREATE_MOVIE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "CREATE_MOVIE_FULFILLED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+      };
+    case "CREATE_MOVIE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "UPDATE_MOVIE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "UPDATE_MOVIE_FULFILLED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+      };
+    case "UPDATE_MOVIE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "CREATE_MOVIE_IMAGE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "CREATE_MOVIE_IMAGE_FULFILLED":
+      return {
+        ...state,
+        movieData: action.payload.data.data,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+      };
+    case "CREATE_MOVIE_IMAGE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "UPDATE_MOVIE_IMAGE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "UPDATE_MOVIE_IMAGE_FULFILLED":
+      return {
+        ...state,
+        movieData: action.payload.data.data,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.msg,
+      };
+    case "UPDATE_MOVIE_IMAGE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "DELETE_MOVIE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "DELETE_MOVIE_FULFILLED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        // msg: action
+      };
+    case "DELETE_MOVIE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "DELETE_MOVIE_IMAGE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "DELETE_MOVIE_IMAGE_FULFILLED":
+      return {
+        ...state,
+        movieData: action.payload.data.data,
+        isLoading: false,
+        isError: false,
+        msg: action.payload.data.data.msg,
+      };
+    case "DELETE_MOVIE_IMAGE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
     default:
       return state;
   }
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,7 +3,9 @@ const initialState = {
   isloading: false,
   isGetError: false,
   isUpdateDataError: false,
+  isUpdateImageError: false,
   isUpdatePasswordError: false,
+  updatedAt: "",
   msg: "",
 };
 
@@ -19,6 +21,7 @@ const user = (state = initialState, action) => {
       return {
         ...state,
         data: action.payload.data.data[0],
+        isLoading: false,
         isGetError: false,
         msg: action.payload.data.msg,
       };
@@ -26,6 +29,7 @@ const user = (state = initialState, action) => {
       return {
         ...state,
         data: {},
+        isLoading: false,
         isGetError: true,
         msg: action.payload.response.data.msg,
       };
@@ -38,7 +42,7 @@ const user = (state = initialState, action) => {
     case "UPDATE_USER_DATA_FULFILLED":
       return {
         ...state,
-        data: action.payload.data.data,
+        data: { ...state.data, ...action.payload.data.data },
         isLoading: false,
         isUpdateDataError: false,
         msg: action.payload.data.msg,
@@ -48,6 +52,32 @@ const user = (state = initialState, action) => {
         ...state,
         isLoading: false,
         isUpdateDataError: true,
+        msg: action.payload.response.data.msg,
+      };
+    case "UPDATE_USER_IMAGE_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isUpdateImageError: false,
+      };
+    case "UPDATE_USER_IMAGE_FULFILLED":
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          user_profile_picture: action.payload.data.data.user_profile_picture,
+        },
+        isLoading: false,
+        isUpdateImageError: false,
+        updatedAt: new Date(Date.now()),
+        msg: action.payload.data.msg,
+      };
+    case "UPDATE_USER_IMAGE_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isUpdateImageError: true,
+        updatedAt: new Date(Date.now()),
         msg: action.payload.response.data.msg,
       };
     case "UPDATE_USER_PASSWORD_PENDING":
