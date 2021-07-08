91d122cf2f2387ac12a8544341ac7bfdc216ac72
twicks95 <teguh.wck95@gmail.com>
2021-07-08 09:34:41 +0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import promiseMiddleware from "redux-promise-middleware";
-import rootReducer from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default createStore(
-  rootReducer,
+import rootReducer from "./reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   applyMiddleware(promiseMiddleware, logger)
 );
+
+export const persistor = persistStore(store);
