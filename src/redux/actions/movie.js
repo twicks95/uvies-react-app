7902e0db37b0f447a8d14f1337d2284d2d187f48
twicks95 <teguh.wck95@gmail.com>
2021-07-08 09:31:55 +0700

    Update movie & user actions and add premiere action

--- src/redux/actions/movie.js
+++ src/redux/actions/movie.js
@@ -1,9 +1,16 @@
 import axiosApiInstances from "../../utils/axios";
 
-export const getMovies = (page, limit) => {
+export const getMovies = (
+  searchByName = "",
+  sort = "movie_id ASC",
+  page = "1",
+  limit = "8"
+) => {
   return {
     type: "GET_MOVIES",
-    payload: axiosApiInstances.get(`movie?page=${page}&limit=${limit}`),
+    payload: axiosApiInstances.get(
+      `movie?searchByName=${searchByName}&sort=${sort}&page=${page}&limit=${limit}`
+    ),
   };
 };
 
@@ -14,9 +21,51 @@ export const getMovieById = (id) => {
   };
 };
 
+export const getUpcomingMovies = (month) => {
+  return {
+    type: "GET_UPCOMING_MOVIES",
+    payload: axiosApiInstances.get(`movie/upcoming/${month}`),
+  };
+};
+
+export const createMovie = (data) => {
+  return {
+    type: "CREATE_MOVIE",
+    payload: axiosApiInstances.post("movie", data),
+  };
+};
+
+export const createMovieImage = (data) => {
+  return {
+    type: "CREATE_MOVIE_IMAGE",
+    payload: axiosApiInstances.post("movie/image", data),
+  };
+};
+
+export const updateMovie = (id, data) => {
+  return {
+    type: "UPDATE_MOVIE",
+    payload: axiosApiInstances.patch(`movie/${id}`, data),
+  };
+};
+
+export const updateMovieImage = (id, data) => {
+  return {
+    type: "UPDATE_MOVIE_IMAGE",
+    payload: axiosApiInstances.patch(`movie/image/${id}`, data),
+  };
+};
+
 export const deleteMovie = (id) => {
   return {
     type: "DELETE_MOVIE",
     payload: axiosApiInstances.delete(`movie/${id}`),
+  };
+};
+
+export const deleteMovieImage = (id) => {
+  return {
+    type: "DELETE_MOVIE_IMAGE",
+    payload: axiosApiInstances.delete(`movie/image/${id}`),
   };
 };
--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -10,13 +10,20 @@ export const getUserData = (id) => {
 export const updateUserData = (id, data) => {
   return {
     type: "UPDATE_USER_DATA",
-    payload: axiosApiInstances.patch(`/user/data/${id}`, data),
+    payload: axiosApiInstances.patch(`user/update/data/${id}`, data),
+  };
+};
+
+export const updateUserImage = (id, data) => {
+  return {
+    type: "UPDATE_USER_IMAGE",
+    payload: axiosApiInstances.patch(`user/data/image/${id}`, data),
   };
 };
 
 export const updateUserPassword = (id, data) => {
   return {
     type: "UPDATE_USER_PASSWORD",
-    payload: axiosApiInstances.patch(`/user/password/${id}`, data),
+    payload: axiosApiInstances.patch(`user/password/${id}`, data),
   };
 };
